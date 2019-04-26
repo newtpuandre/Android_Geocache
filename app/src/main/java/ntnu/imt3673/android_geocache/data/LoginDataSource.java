59765c37ccf253fabe4a681d201fe4c95209d68b
newtpuandre <andgun96@gmail.com>
2019-04-26 21:10:43 +0200

    Prepared login system for real API data

--- app/src/main/java/ntnu/imt3673/android_geocache/data/LoginDataSource.java
+++ app/src/main/java/ntnu/imt3673/android_geocache/data/LoginDataSource.java
@@ -1,8 +1,14 @@
 package ntnu.imt3673.android_geocache.data;
 
-import java.io.IOException;
+import android.util.Log;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import ntnu.imt3673.android_geocache.api.ApiHandler;
+import ntnu.imt3673.android_geocache.api.model.TestData;
 import ntnu.imt3673.android_geocache.data.model.LoggedInUser;
+import retrofit2.Call;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves User information.
@@ -13,15 +19,16 @@ public class LoginDataSource {
 
         final Boolean LoggedIn = true;
 
-        /*ApiHandler.TaskService taskService = ApiHandler.createService(ApiHandler.TaskService.class);
+        ApiHandler.TaskService taskService = ApiHandler.createService(ApiHandler.TaskService.class);
         Thread t = new Thread();
         Call<ArrayList<TestData>> call = taskService.getTestData();
         ArrayList<TestData> data = null;
         try {
             data = call.execute().body();
+            Log.d("app1", data.toString());
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
+        }
 
 
         LoggedInUser fakeUser = new LoggedInUser(java.util.UUID.randomUUID().toString(),
