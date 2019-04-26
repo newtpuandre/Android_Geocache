157bddb5eab6d7fc968f0098f447c6d7471eba7d
newtpuandre <andgun96@gmail.com>
2019-04-26 19:59:44 +0200

    API calls and models partily implemented

--- app/src/main/java/ntnu/imt3673/android_geocache/data/LoginDataSource.java
+++ app/src/main/java/ntnu/imt3673/android_geocache/data/LoginDataSource.java
@@ -1,26 +1,37 @@
 package ntnu.imt3673.android_geocache.data;
 
-import ntnu.imt3673.android_geocache.data.model.LoggedInUser;
-
 import java.io.IOException;
 
+import ntnu.imt3673.android_geocache.data.model.LoggedInUser;
+
 /**
- * Class that handles authentication w/ login credentials and retrieves user information.
+ * Class that handles authentication w/ login credentials and retrieves User information.
  */
 public class LoginDataSource {
 
-    public Result<LoggedInUser> login(String username, String password) {
+    public Result<LoggedInUser> login(final String username, final String password) {
 
+        final Boolean LoggedIn = true;
+
+        /*ApiHandler.TaskService taskService = ApiHandler.createService(ApiHandler.TaskService.class);
+        Thread t = new Thread();
+        Call<ArrayList<TestData>> call = taskService.getTestData();
+        ArrayList<TestData> data = null;
         try {
-            // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
+            data = call.execute().body();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+
+        LoggedInUser fakeUser = new LoggedInUser(java.util.UUID.randomUUID().toString(),
+                "Test Testesen");
+        if (LoggedIn) {
             return new Result.Success<>(fakeUser);
-        } catch (Exception e) {
-            return new Result.Error(new IOException("Error logging in", e));
+        } else {
+            return new Result.Error(new IOException("Error logging in"));
         }
+
     }
 
     public void logout() {
