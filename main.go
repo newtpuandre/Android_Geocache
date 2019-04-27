0697515e74180ffa23254e660637251a2e6ca8bf
bgwendling <bgwendling@gmail.com>
2019-04-27 15:41:45 +0200

    Fixed bug in structs, error in naming convention of variables, struct variables now public

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 	"time"
 	"net/http"
-	
+
 	"github.com/rs/xid"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -25,38 +25,32 @@ var startTime time.Time
 var client mongo.Client
 
 type Message struct {
-	messageID	string		`json:"messageID"`
-	timestamp	string		`json:"timestamp"`
-	userID		string		`json:"userID"`
-	message		string		`json:"message"`
-	imageURL	string		`json:"imageURL"`
-	long		float32		`json:"long"`
-	lat			float32		`json:"lat"`
-	mType		int 		`json:"mType"`	//private/friend/public messages
+	MessageID	string		`json:"messageID"`
+	Timestamp	string		`json:"timestamp"`
+	UserID		string		`json:"userID"`
+	Message		string		`json:"message"`
+	ImageURL	string		`json:"imageURL"`
+	Long		float32		`json:"long"`
+	Lat			float32		`json:"lat"`
+	MType		int 		`json:"mType"`	//private/friend/public messages
 }
 
 type User struct {
-	userID		string		`json:"userID"`
-	userName	string		`json:"userName"`
-	passHash	string		`json:"passHash"`
-	friendIDs	[]string	`json:"friendIDs"`
+	UserID		string		`json:"userID"`
+	UserName	string		`json:"userName"`
+	PassHash	string		`json:"passHash"`
+	FriendIDs	[]string	`json:"friendIDs"`
 }
 
 type MessageRequest struct {
-	userID		string		`json:"userID"`
-	long		float32		`json:"long"`
-	lat			float32		`json:"lat"`
+	UserID		string		`json:"userID"`
+	Long		float32		`json:"long"`
+	Lat			float32		`json:"lat"`
 }
 
 func getMessages(c *gin.Context) {
-	type position struct {
-		long		float32		`json:"long"`
-		lat			float32		`json:"lat"`
-	}
-	
 	var messageRequest MessageRequest
 	c.BindJSON(&messageRequest)
-
 	/*mdb := client.Database("map_messages").Collection("Messages")
 
 	mdb.Aggregate(
@@ -66,25 +60,28 @@ func getMessages(c *gin.Context) {
 	)*/
 
 	//dummy reply
-	var messages [2]Message
-		messages [0] = Message{
-			"TestMessage1",
-			time.Now().String(),
-			"DummyUser",
-			"This is the first test message it has an imageURL",
-			"https://images.pexels.com/photos/2156311/pexels-photo-2156311.jpeg",
-			messageRequest.long,
-			messageRequest.lat,
-			0}
-		messages [1] = Message{
-			"TestMessage2",
-			time.Now().String(),
-			"UserDummy",
-			"This is the first test message it does NOT have an imageURL",
-			"",
-			messageRequest.long + 0.001,
-			messageRequest.lat + 0.001,
-			0}
+	messages := []Message{
+		Message{
+			MessageID: 	"TestMessage1",
+			Timestamp: 	time.Now().String(),
+			UserID: 	"DummyUser",
+			Message: 	"This is the first test message it has an imageURL",
+			ImageURL:	"https://images.pexels.com/photos/2156311/pexels-photo-2156311.jpeg",
+			Long:		messageRequest.Long,
+			Lat:		messageRequest.Lat,
+			MType:		0,
+		},
+		Message{
+			MessageID: 	"TestMessage2",
+			Timestamp: 	time.Now().String(),
+			UserID: 	"UserDummy",
+			Message: 	"This is the second test message it does NOT have an imageURL",
+			ImageURL:	"",
+			Long:		messageRequest.Long + 0.001,
+			Lat:		messageRequest.Lat + 0.001,
+			MType:		0,
+		},
+	}
 	//end of dummy reply
 
 	c.JSON(http.StatusOK, messages)
@@ -94,8 +91,8 @@ func postMessage(c *gin.Context) {
 	var message Message
 	c.BindJSON(&message)
 
-	message.messageID = xid.New().String()
-	message.timestamp = time.Now().String()
+	message.MessageID = xid.New().String()
+	message.Timestamp = time.Now().String()
 
 	_, err := client.Database("map_messages").Collection("Messages").InsertOne(context.TODO(), message);
 	if err != nil {
@@ -129,7 +126,7 @@ func postUser(c *gin.Context) {
 	var user User
 	c.BindJSON(&user)
 
-	user.userID = xid.New().String()
+	user.UserID = xid.New().String()
 
 	_, err := client.Database("map_messages").Collection("Users").InsertOne(context.TODO(), user)
 	if err != nil {
